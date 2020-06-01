d2087fe42ef7cc9452e4328c7f62ddb304a55390
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-06-01 17:59:33 -0300

    :lipstick: feat: Created colors and fonts styles files

---
+++ src/styles/settings/colors.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const Colors = createGlobalStyle`
+    :root {
+        --base-color: #A82108;
+        --secondary-color: #F2931B;
+        --support-color-1: #F2F2F2;
+        --support-color-2: #DDDFD6;
+        --support-color-3: #FFFFFF;
+        --support-color-4: #10212B;
+        --support-color-5: #203744;
+        --support-color-6: #36505E;
+    }
+
+`;
+
+export default Colors;

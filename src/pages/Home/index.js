e3863e996e3cccc0365e24d2c50644d6b4657a98
Matheusdoe-dev\n <matheusdoe.dev@gmail.com>
2020-07-08 18:07:46 +0000

    :lipstick: chore: Styled Home page component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,44 +1,65 @@
 import React from "react";
+// components
 import { Container, Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
-
-import brandImg from "../../assets/icons/brand.svg";
+import Header from "../../components/Header/index";
+import CTASection from "../../components/CTA-Section/index";
+import Footer from "../../components/Footer/index";
+// style
+import { Hero, Intro } from "./styles";
+// imgs
+import introImg from "../../assets/imgs/home-intro-img.jpg";
 
 const Index = () => {
   return (
     <>
-      <header>
+      <Header />
+
+      <Hero>
         <Container>
           <Row>
-            <Col xs="4">
-              <Link to="/" className="brand">
-                <img src={brandImg} href="/" alt="Muzzarella" />
-              </Link>
-            </Col>
-            <Col xs="8">
-              <nav>
-                <ul>
-                  <li>
-                    <Link to="">Home</Link>
-                  </li>
-                  <li>
-                    <Link to="">Menu</Link>
-                  </li>
-                  <li>
-                    <Link to="">About</Link>
-                  </li>
-                  <li>
-                    <Link to="">FAQ</Link>
-                  </li>
-                  <li>
-                    <Link to="">Order</Link>
-                  </li>
-                </ul>
-              </nav>
+            <Col xs="12">
+              <h1>Muzzarella</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod
+              </p>
             </Col>
           </Row>
         </Container>
-      </header>
+      </Hero>
+
+      <Intro>
+        <Container>
+          <Row style={{ alignItems: "center" }}>
+            <Col xs="12" sm="7">
+              <h3>Our own food order delivery system</h3>
+              <p>
+                is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s, when an unknown printer took a galley of type
+                and scrambled it to make
+              </p>
+
+              <p>
+                is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s, when an unknown printer took a galley of type
+                and scrambled it to make
+              </p>
+            </Col>
+            <Col xs="12" sm="5">
+              <img
+                src={introImg}
+                alt="Imagem de introdução, entregador de pizza"
+              />
+            </Col>
+          </Row>
+        </Container>
+      </Intro>
+
+      <CTASection />
+
+      <Footer />
     </>
   );
 };
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import heroImg from "../../assets/imgs/bg-main.jpg";
+
+export const Hero = styled.div`
+  & {
+    background-image: url(${heroImg});
+    background-clip: cover;
+    height: 628px;
+
+    h1,
+    p {
+      text-align: center;
+      color: var(--support-color-1);
+    }
+
+    h1 {
+      margin-top: calc(var(--gap-lg) * 2);
+    }
+
+    p {
+      font-size: 1.375rem;
+      margin: var(--gap) auto 0 auto;
+    }
+  }
+`;
+
+export const Intro = styled.section`
+  & {
+    background: var(--support-color-1);
+    padding: var(--gap-xl) 0;
+
+    p {
+      margin-top: var(--gap);
+    }
+
+    p,
+    h3 {
+      color: var(--base-color);
+    }
+
+    img {
+      border-radius: 50px;
+    }
+  }
+`;

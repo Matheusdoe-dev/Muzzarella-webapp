82520119e49653ac08d497d650d4528cdd8c1931
Matheusdoe-dev\n <matheusdoe.dev@gmail.com>
2020-07-08 18:05:19 +0000

    :construction: feat: Created styles files
    elements.js, container.js and gaps.js

---
+++ src/styles/base/elements.js
@@ -0,0 +1,55 @@
+import { createGlobalStyle } from "styled-components";
+
+const Elements = createGlobalStyle`  
+    h1, h2, h3, h4, h5, h6 {
+        font-family: var(--font-primary);
+        font-weight: 400;
+        line-height: 1.10em;
+    }
+
+    h1 {
+        font-size: 5.93rem;
+    }
+
+    h2 {
+        font-size: 3.68rem;
+    }
+
+    h3 {
+        font-size: 2.25rem;
+    }
+
+    h4 {
+        font-size: 1.37rem;
+    }
+
+    p, input, label, a, button, textarea {
+        font-family: var(--font-secondary);
+    }
+
+    p, input, label, textarea {
+        font-size: .875rem;
+        filter: brightness(85%);
+    }
+
+    p, textarea {
+        max-width: 30em;
+        line-height: 1.55em;
+    }
+
+    a, button {
+        font-size: 1.37rem;
+        filter: brightness(95%);
+    }
+
+    a {
+        text-decoration: none;
+        color: initial;
+
+        :hover {
+            text-decoration: none;
+        }
+    }
+`;
+
+export default Elements;
---
+++ src/styles/objects/container.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import ContainerBootstrap from "react-bootstrap/Container";
+
+export const Container = styled(ContainerBootstrap)`
+  & {
+    max-width: 1180px;
+  }
+`;

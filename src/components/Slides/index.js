1e703d5d96176c9b7bdcac806f4a5b4407ff5096
Matheusdoe-dev\n <matheusdoe.dev@gmail.com>
2020-07-09 20:47:01 +0000

    :lipstick: feat: Created slides component

---
+++ src/components/Slides/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+// components
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+// imgs
+import slidesImg1 from "../../assets/imgs/slides-image-1.jpg";
+import slidesImg2 from "../../assets/imgs/slides-image-2.jpg";
+import slidesImg3 from "../../assets/imgs/slides-image-3.jpg";
+import slidesImg4 from "../../assets/imgs/slides-image-4.jpg";
+// styled
+import { SlidesWrapper } from "./styles";
+
+const Slides = () => {
+  return (
+    <>
+      <SlidesWrapper>
+        <Container>
+          <Row>
+            <Col xs="12" sm="3">
+              <img src={slidesImg1} alt="" />
+            </Col>
+            <Col xs="12" sm="3">
+              <img src={slidesImg2} alt="" />
+            </Col>
+            <Col xs="12" sm="3">
+              <img src={slidesImg3} alt="" />
+            </Col>
+            <Col xs="12" sm="3">
+              <img src={slidesImg4} alt="" />
+            </Col>
+          </Row>
+        </Container>
+      </SlidesWrapper>
+    </>
+  );
+};
+
+export default Slides;
---
+++ src/components/Slides/styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const SlidesWrapper = styled.div`
+  & img {
+    border-radius: 50px;
+    padding: var(--gap-xl) 0;
+  }
+`;

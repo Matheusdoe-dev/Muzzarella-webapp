ea0fcaccf5b8920458313796c69be5b18b1b1918
Matheusdoe-dev\n <matheusdoe.dev@gmail.com>
2020-07-09 20:45:33 +0000

    :lipstick: feat: Created About us page component

---
+++ src/pages/About-us/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+// imgs
+import footerImg from "../../assets/imgs/footer-img-aboutus.jpg";
+
+export const AboutUsSection = styled.section`
+  & {
+    padding: var(--gap-xl);
+
+    h4 {
+      margin-bottom: var(--gap-sm);
+    }
+  }
+`;
+
+export const FooterImg = styled.div`
+  & {
+    background: url(${footerImg});
+    height: 80px;
+  }
+`;

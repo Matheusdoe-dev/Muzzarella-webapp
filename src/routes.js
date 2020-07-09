3f4a8c0e549a95e2b0fecac91f8f31a01cd00902
Matheusdoe-dev\n <matheusdoe.dev@gmail.com>
2020-07-09 20:48:07 +0000

    :construction: chore: Added new routes
    /menu , /aboutus , /aboutmuzzarella , /faq

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,20 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 // routes
 import Index from "./pages/Home/index";
+import Menu from "./pages/Menu/index";
+import AboutUs from "./pages/About-us/index";
+import AboutMuzzarella from "./pages/About-Muzzarella/index";
+import FAQ from "./pages/FAQ/index";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Index} />
+        <Route path="/menu" component={Menu} />
+        <Route path="/aboutus" component={AboutUs} />
+        <Route path="/aboutmuzzarella" component={AboutMuzzarella} />
+        <Route path="/faq" component={FAQ} />
       </Switch>
     </BrowserRouter>
   );

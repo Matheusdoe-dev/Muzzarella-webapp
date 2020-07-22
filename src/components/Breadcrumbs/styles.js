c04b09e1f9a135ccba4d07a6069863b93aa44f46
Matheusdoe-dev\n <matheusdoe.dev@gmail.com>
2020-07-22 21:10:01 +0000

    :construction: fix: Minor fixes
    Fixed Link import console bug and page title h3 align

--- src/components/Breadcrumbs/styles.js
+++ src/components/Breadcrumbs/styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 // components
-import Link from "react-router-dom/Link";
+import { Link } from "react-router-dom";
 
 export const BreadcrumbsWrapper = styled.div`
   & {
--- src/components/Page-title/index.js
+++ src/components/Page-title/index.js
@@ -1,21 +1,11 @@
 import React from "react";
-// components
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 // styled
 import { PageTitleWrapper } from "./styles";
 
 const PageTitle = ({ title, background }) => {
   return (
     <PageTitleWrapper bg={background}>
-      <Container>
-        <Row style={{ alignItems: "center" }}>
-          <Col xs="12">
-            <h3>{title}</h3>
-          </Col>
-        </Row>
-      </Container>
+      <h3>{title}</h3>
     </PageTitleWrapper>
   );
 };

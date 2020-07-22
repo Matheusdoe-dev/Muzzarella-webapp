d284876690a6f8aa91fd57f6b5ab0ed84836c365
Matheusdoe-dev\n <matheusdoe.dev@gmail.com>
2020-07-22 21:08:09 +0000

    :lipstick: feat: OrderEnd page component created

---
+++ src/pages/Order-End/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+// components
+import { Container, Row, Col } from "react-bootstrap";
+import Header from "../../components/Header/index";
+import PageTitle from "../../components/Page-title/index";
+import Footer from "../../components/Footer/index";
+import Breadcrumbs from "../../components/Breadcrumbs/index";
+// styled-components
+import { OrderEndWrapper } from "./styles";
+// imgs
+import pageTitleImg from "../../assets/imgs/order/bg-page-end.jpg";
+
+const OrderCheckout = () => {
+  return (
+    <>
+      <Header />
+      <PageTitle background={pageTitleImg} title="Thanks!" />
+      <Breadcrumbs />
+
+      <OrderEndWrapper>
+        <Container>
+          <Row>
+            <Col xs="12">
+              <h4>Thanks for order with us. :)</h4>
+              <p>Check your email with the order details</p>
+
+              <span>Contact us for more questions and suggestions.</span>
+            </Col>
+          </Row>
+        </Container>
+      </OrderEndWrapper>
+
+      <Footer />
+    </>
+  );
+};
+
+export default OrderCheckout;
---
+++ src/pages/Order-End/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const OrderEndWrapper = styled.form`
+  & {
+    padding: var(--gap-xl) 0;
+    text-align: center;
+
+    h4 {
+      color: var(--base-color);
+    }
+
+    p {
+      margin: 0 auto;
+      margin-bottom: var(--gap);
+      font-size: 1rem;
+    }
+  }
+`;

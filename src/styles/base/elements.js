9d3f15e0844ddc5a2cf7df87db80f23cb3ef12bc
Matheusdoe-dev\n <matheusdoe.dev@gmail.com>
2020-07-23 20:51:45 +0000

    :lipstick: fix: Fixing imgs size

--- src/styles/base/elements.js
+++ src/styles/base/elements.js
@@ -5,6 +5,12 @@ const Elements = createGlobalStyle`
         background: var(--support-color-1);
     }
 
+    img {
+        max-width: 100%;
+        display: block;
+        border-radius: 50px;
+    }
+
     h1, h2, h3, h4, h5, h6 {
         font-family: var(--font-primary);
         font-weight: 400;
@@ -13,18 +19,42 @@ const Elements = createGlobalStyle`
 
     h1 {
         font-size: 5.93rem;
+
+        @media (max-width: 767.98px) {
+            & {
+                font-size: 2.875rem;
+            }
+        }
     }
 
     h2 {
         font-size: 3.68rem;
+
+        @media (max-width: 767.98px) {
+           & {
+               font-size: 2.25rem; 
+            }
+        }
     }
 
     h3 {
         font-size: 2.25rem;
+
+        @media (max-width: 767.98px) {
+           & {
+               font-size: 1.5rem; 
+            }
+        }
     }
 
     h4 {
         font-size: 1.37rem;
+
+        @media (max-width: 767.98px) {
+           & {
+               font-size: 1.1.25rem; 
+            }
+        }
     }
 
     p, input, label, a, button, textarea, span, li {
@@ -45,6 +75,12 @@ const Elements = createGlobalStyle`
     a, button {
         font-size: 1.37rem;
         filter: brightness(95%);
+
+        @media (max-width: 767.98px) {
+           & {
+               font-size: .875rem; 
+            }
+        }
     }
 
     a {

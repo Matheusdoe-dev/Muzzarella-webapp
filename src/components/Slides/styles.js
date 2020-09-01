4dd90f66182621f673492527577b4d2a35a44f2a
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-09-01 14:59:38 -0300

    :bug: chore: Slides padding section fixed

--- src/components/Slides/styles.js
+++ src/components/Slides/styles.js
@@ -2,9 +2,10 @@ import styled from "styled-components";
 
 export const SlidesWrapper = styled.div`
   & {
+    padding: var(--gap-xl) 0;
+
     img {
       border-radius: 50px;
-      padding: var(--gap-xl) 0;
     }
 
     @media (max-width: 767.98px) {

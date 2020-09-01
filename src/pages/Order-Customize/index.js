db0ab219c0cec40d541e68efdb6e91d53e7039ee
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-09-01 11:04:39 -0300

    :lipstick: chore: putting Checkbox-List component on Order-Customize page

--- src/pages/Order-Customize/index.js
+++ src/pages/Order-Customize/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 // components
 import { Container, Row, Col } from "react-bootstrap";
 import Header from "../../components/Header/index";
@@ -6,128 +7,101 @@ import PageTitle from "../../components/Page-title/index";
 import Footer from "../../components/Footer/index";
 import Breadcrumbs from "../../components/Breadcrumbs/index";
 // styled-components
-import { OrderCustomizeWrapper, Checkmark } from "./styles";
+import { OrderCustomizeWrapper } from "./styles";
 import { SubmitButton } from "../../styles/objects/button";
+import CheckboxList from "../../components/Checkbox-List";
 // imgs
 import pageTitleImg from "../../assets/imgs/order/bg-page-choose-premade.jpg";
 
 const OrderCustomize = () => {
+  const history = useHistory();
+
   return (
     <>
       <Header />
       <PageTitle background={pageTitleImg} title="Choose a premade option" />
       <Breadcrumbs />
 
-      <OrderCustomizeWrapper>
+      <OrderCustomizeWrapper onSubmit={() => history.push("/order/review")}>
         <Container>
           <Row>
             <Col xs="12">
               <h4>Pasta</h4>
             </Col>
-            <Col xs="12" sm="3">
-              <Checkmark>
-                <label for="slim">
-                  <input type="radio" id="slim" name="pasta" checked />
-                  <span></span>
-                  <p>Slim</p>
-                </label>
-              </Checkmark>
-            </Col>
-            <Col xs="12" sm="3">
-              <Checkmark>
-                <label for="average">
-                  <input type="radio" id="average" name="pasta" />
-                  <span></span>
-                  <p>Average</p>
-                </label>
-              </Checkmark>
-            </Col>
-            <Col xs="12" sm="3">
-              <Checkmark>
-                <label for="thick">
-                  <input type="radio" id="thick" name="pasta" />
-                  <span></span>
-                  <p>Thick</p>
-                </label>
-              </Checkmark>
-            </Col>
+            <CheckboxList
+              name="pasta"
+              type="radio"
+              options={[
+                { name: "slim", label: "Slim" },
+                { name: "average", label: "Average" },
+                { name: "thick", label: "Thick" },
+              ]}
+            />
           </Row>
           <Row>
             <Col xs="12">
               <h4>Size</h4>
             </Col>
-            <Col xs="12" sm="3">
-              <Checkmark>
-                <label for="small">
-                  <input type="radio" id="small" name="size" checked />
-                  <span></span>
-                  <p>Small</p>
-                </label>
-              </Checkmark>
-            </Col>
-            <Col xs="12" sm="3">
-              <Checkmark>
-                <label for="medium">
-                  <input type="radio" id="medium" name="size" />
-                  <span></span>
-                  <p>Medium</p>
-                </label>
-              </Checkmark>
-            </Col>
-            <Col xs="12" sm="3">
-              <Checkmark>
-                <label for="big">
-                  <input type="radio" id="big" name="size" />
-                  <span></span>
-                  <p>Big</p>
-                </label>
-              </Checkmark>
-            </Col>
-            <Col xs="12" sm="3">
-              <Checkmark>
-                <label for="family">
-                  <input type="radio" id="family" name="size" />
-                  <span></span>
-                  <p>Family</p>
-                </label>
-              </Checkmark>
-            </Col>
+            <CheckboxList
+              name="size"
+              type="radio"
+              options={[
+                { name: "small", label: "Small" },
+                { name: "medium", label: "Medium" },
+                { name: "big", label: "Big" },
+                { name: "family", label: "Family" },
+              ]}
+            />
           </Row>
           <Row>
             <Col xs="12">
               <h4>Sauce</h4>
             </Col>
-            <Col xs="12" sm="3">
-              <Checkmark>
-                <label for="traditional-tomato">
-                  <input
-                    type="checkbox"
-                    id="traditional-tomato"
-                    name="sauce"
-                    checked
-                  />
-                  <span className="checkbox"></span>
-                  <p>Traditional Tomato</p>
-                </label>
-              </Checkmark>
+            <CheckboxList
+              name="sauce"
+              type="checkbox"
+              options={[
+                { name: "traditional-tomato", label: "Traditional Tomato" },
+                { name: "gorgonzola-sauce", label: "Gorgonzola Sauce" },
+                { name: "parisian-sauce", label: "Parisian Sauce" },
+              ]}
+            />
+          </Row>
+          <Row>
+            <Col xs="12">
+              <h4>Pizza Topping</h4>
             </Col>
-            <Col xs="12" sm="3">
-              <Checkmark>
-                <label for="gorgonzola-sauce">
-                  <input type="checkbox" id="gorgonzola-sauce" name="sauce" />
-                  <span className="checkbox"></span>
-                  <p>Gorgonzola Sauce</p>
-                </label>
-              </Checkmark>
-            </Col>
-            <Col xs="12" sm="3">
-              <Checkmark>
-                <label for="parisian-sauce">
-                  <input type="checkbox" id="parisian-sauce" name="sauce" />
-                  <span className="checkbox"></span>
-                  <p>Parisian Sauce</p>
-                </label>
-              </Checkmark>
+            <CheckboxList
+              name="topping"
+              type="checkbox"
+              options={[
+                { name: "mozzarella", label: "Mozzarella" },
+                { name: "parmesan", label: "Parmesan" },
+                { name: "provolone", label: "Provolone" },
+                { name: "catupiry", label: "Catupiry" },
+                { name: "tomato", label: "Tomato" },
+                { name: "oregano", label: "Oregano" },
+                { name: "olives", label: "Olives" },
+                { name: "onion", label: "Onion" },
+                { name: "ham", label: "Ham" },
+                { name: "pepperoni", label: "Pepperoni" },
+                { name: "sausage", label: "Sausage" },
+                { name: "black-olives", label: "Black olives" },
+                { name: "garlic", label: "Garlic" },
+                { name: "olive-oil", label: "Olive oil" },
+                { name: "anchovies", label: "Anchovies" },
+              ]}
+            />
+          </Row>
+          <Row>
+            <Col xs="12">
+              <SubmitButton
+                width="372px"
+                background="var(--base-color)"
+                color="var(--support-color-1)"
+              >
+                Confirm order
+              </SubmitButton>
             </Col>
           </Row>
         </Container>

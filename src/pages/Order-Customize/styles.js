44b09a92f3ee2cb6202eab306ae89dd2b52ee7d6
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-09-01 11:05:15 -0300

    :lipstick: chore: input and label styles moved to Input component folder in styles file

--- src/pages/Order-Customize/styles.js
+++ src/pages/Order-Customize/styles.js
@@ -24,87 +24,19 @@ export const OrderCustomizeWrapper = styled.form`
 
       h4 {
         text-align: center;
-        margin-bottom: var(--gap-sm);
+        margin-bottom: var(--gap-md);
       }
     }
-  }
-`;
 
-export const Checkmark = styled.div`
-  & {
-    label {
-      display: block;
-      position: relative;
-      padding-left: 35px;
-      margin-bottom: 12px;
-      height: 71px;
-      cursor: pointer;
-      font-size: 22px;
-      -webkit-user-select: none;
-      -moz-user-select: none;
-      -ms-user-select: none;
-      user-select: none;
-
-      input {
-        position: absolute;
-        opacity: 0;
-        cursor: pointer;
-        height: 0px;
-        width: 0px;
-      }
-
-      &:hover input ~ span {
-        background-color: var(--support-color-1);
-      }
-
-      input:checked ~ span {
-        background-color: var(--secondary-color);
-      }
-      /*
-      input:checked ~ span:after {
-        display: block;
-      }
-
-      span:after {
-        left: 9px;
-        top: 5px;
-        width: 10px;
-        height: 20px;
-        border: 3px solid var(--base-color);
-        border-width: 0 3px 3px 0;
-        -webkit-transform: rotate(45deg);
-        -ms-transform: rotate(45deg);
-        transform: rotate(45deg);
-      } */
-    }
-
-    span {
-      position: absolute;
-      top: 0;
-      left: 0;
-      height: 71px;
-      width: 71px;
-      border-radius: 50px;
-      border: 3px solid var(--support-color-4);
-      background-color: var(--support-color-1);
-
-      &:after {
+    .row:last-child {
+      &::before {
         content: "";
-        position: absolute;
         display: none;
       }
     }
 
-    .checkbox {
-      border-radius: 0 !important;
-    }
-
-    p {
-      position: absolute;
-      top: 20px;
-      font-size: 1.125rem;
-      margin-left: calc(var(--gap-lg) - 12px);
-      /* margin-top: var(--gap); */
+    button {
+      margin-top: var(--gap-md);
     }
   }
 `;

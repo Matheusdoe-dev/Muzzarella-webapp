a69be15bdc16d5bc90e4ade756062473ac388733
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-09-03 15:17:05 -0300

    :construction: chore: users route created

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,18 @@ const express = require("express");
 const routes = express.Router();
 // controllers
 const OrderController = require("./controllers/OrderController");
+const UserController = require("./controllers/UserController");
 
+// create order
 routes.post("/orders", OrderController.create);
+
+// index all orders
+routes.get("/orders", OrderController.indexAll);
+
+// create user
+routes.post("/users", UserController.create);
+
+// index all users
+routes.get("/users", UserController.indexAll);
 
 module.exports = routes;

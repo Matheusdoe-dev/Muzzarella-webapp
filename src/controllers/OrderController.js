6f7d29187fc6bbc63df87be72b2a79364ce8d815
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-09-03 15:18:38 -0300

    :sparkles: chore: index all orders feature created

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -1,9 +1,11 @@
 const knex = require("../database/connection");
 
 const OrderController = {
+  // create order funcionality
   async create(req, res) {
     try {
       const {
+        user_id,
         card_number,
         card_valid,
         cvv,
@@ -16,6 +18,7 @@ const OrderController = {
       } = await req.body;
 
       const order = {
+        user_id,
         card_number,
         card_valid,
         cvv,
@@ -32,6 +35,17 @@ const OrderController = {
       const orderId = insertedOrders[0];
 
       return res.json({ orderId });
+    } catch (error) {
+      console.log(error);
+      return res.json({ error });
+    }
+  },
+
+  async indexAll(req, res) {
+    try {
+      const orders = await knex("orders").select("*");
+
+      return res.json([...orders]);
     } catch (error) {
       console.log(error);
       return res.json({ error });

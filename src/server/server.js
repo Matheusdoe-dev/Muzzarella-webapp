15ebc18a7c3a35db980634d7dd7102b842a8c3cd
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-09-04 16:12:24 -0300

    :art: chore: app and server splited

--- src/server/server.js
+++ src/server/server.js
@@ -1,14 +1,6 @@
-const express = require("express");
-const cors = require("cors");
-const routes = require("../routes");
-
-const app = express();
+const app = require("./../app");
 
 const port = process.env.PORT || 3333;
-
-app.use(cors());
-app.use(express.json());
-app.use(routes);
 
 app.listen(port, () => {
   console.log(`Rodando na porta ${port}`);

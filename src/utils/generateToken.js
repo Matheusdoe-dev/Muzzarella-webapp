545d688d25e415d39f09981289c2481394f5959f
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-09-11 17:33:48 -0300

    :art: chore: some comments added

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const authConfig = require("../config/auth.json");
 
+// jwt token generator
 const generateToken = (params = {}) => {
   return jwt.sign(params, authConfig.secret, {
     expiresIn: 86400,

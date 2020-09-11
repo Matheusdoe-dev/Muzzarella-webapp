f1cbe7a53264557a84f0fa848f3d0c2370bca12c
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-09-11 17:33:23 -0300

    :wrench: chore: Putting console.log inside callback on app.listen in english

--- src/server/server.js
+++ src/server/server.js
@@ -3,5 +3,5 @@ const app = require("./../app");
 const port = process.env.PORT || 3333;
 
 app.listen(port, () => {
-  console.log(`Rodando na porta ${port}`);
+  console.log(`Server running on ${port}`);
 });

40469ebc1b56887482c95674d789f59a3ea2d4f4
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-10-06 19:57:16 -0300

    :sparkles: feat: Order creation functionality created

---
+++ src/hooks/order.js
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+// apis
+import muzzarellaApi from "../services/muzzarella-api";
+
+const useOrderHook = () => {
+  // order way
+  const [orderWay, setOrderWay] = useState("");
+
+  // order checkout data
+  const [items, setItems] = useState([]);
+  const [card_number, setCardNumber] = useState("");
+  const [card_valid, setCardValid] = useState("");
+  const [cvv, setCVV] = useState("");
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
+  const [adress, setAdress] = useState("");
+  const [city, setCity] = useState("");
+  const [zip, setZipCode] = useState("");
+  const [price, setPrice] = useState("R$ 32,00");
+
+  const history = useHistory();
+
+  const handleOrderWay = (event) => {
+    if (event.target.innerHTML.includes("Customize")) {
+      setOrderWay("customize");
+    } else {
+      setOrderWay("premade");
+    }
+  };
+
+  const handleItems = (event, value, type) => {
+    if (event.currentTarget.checked === true) {
+      setItems([...items, { item: value, type: type }]);
+    } else {
+      setItems(items.filter((item) => item.item !== value));
+    }
+    console.log(items);
+  };
+
+  const handleChecked = (value, data) => {
+    return data.includes(value);
+  };
+
+  // handle order submit function
+  const handleSubmitOrder = async (event) => {
+    event.preventDefault();
+
+    try {
+      await muzzarellaApi
+        .post(
+          "/orders",
+          {
+            items,
+            card_number,
+            card_valid,
+            cvv,
+            first_name,
+            last_name,
+            adress,
+            city,
+            zip,
+            price,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        )
+        .then((r) => r.data);
+      alert("Order created, thanks to use our services!");
+      history.push("/order/end");
+    } catch (error) {
+      alert("Unable to create order, try again.");
+    }
+  };
+
+  return {
+    orderWay,
+    handleOrderWay,
+    items,
+    setItems,
+    card_number,
+    setCardNumber,
+    card_valid,
+    setCardValid,
+    cvv,
+    setCVV,
+    first_name,
+    setFirstName,
+    last_name,
+    setLastName,
+    adress,
+    setAdress,
+    city,
+    setCity,
+    zip,
+    setZipCode,
+    handleSubmitOrder,
+    handleItems,
+    handleChecked,
+  };
+};
+
+export default useOrderHook;

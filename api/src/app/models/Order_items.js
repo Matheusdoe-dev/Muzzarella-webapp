8e09c75f8b9f102d22a5411bc67416e43f822246
matheusdoedev <matheusdoe.dev@gmail.com>
2023-08-07 12:02:58 -0300

    feat: rebooting api project with nestjs

--- api/src/app/models/Order_items.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const Order_items = sequelize.define("Order_items", {
-    item: DataTypes.STRING,
-    type: DataTypes.STRING,
-    order_id: DataTypes.NUMBER,
-  });
-
-  return Order_items;
-};

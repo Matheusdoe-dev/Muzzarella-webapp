70001fcaf7628bea58f0189ac321d6ec17ad0662
matheusdoedev <matheusdoe.dev@gmail.com>
2023-08-14 13:55:17 -0300

    feat: developing

---
+++ web/src/app/menu/page.tsx
@@ -0,0 +1,5 @@
+import { Menu } from "@/views";
+
+const MenuPage = () => <Menu />;
+
+export default MenuPage;
---
+++ web/src/components/InternPageCover/InternPageCover.tsx
@@ -0,0 +1,3 @@
+const InternPageCover = ({ url }) => {
+  return <div style={{ backgroundImage: url }}></div>;
+};
